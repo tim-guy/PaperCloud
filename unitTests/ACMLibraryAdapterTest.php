69534cea97bffebccae7e8ca14349b176ff73c3c
Timmy Malaney <tmalaney@usc.edu>
2017-04-03 16:25:04 -0700

    whitebox for IEEE and ACM complete

--- unitTests/ACMLibraryAdapterTest.php
+++ unitTests/ACMLibraryAdapterTest.php
@@ -1,12 +1,30 @@
 <?php
-//require_once "cache/CacheArtistMBIDs.php";
+require_once "vendor/phpunit/phpunit/src/Framework/Assert/Functions.php";
+require_once "ajax/ACMLibraryAdapter.php";
 
 use PHPUnit\Framework\TestCase;
 
 class ACMLibraryAdapterTest extends TestCase
 {
-	public function testFunction()
+	public function testValidGetPapersWithAuthorName()
 	{
-		
+		$expectedOutputFileName = "unitTests/expected_output/testValidACMGetPapersWithAuthorNameOutput.json";
+		$ACM = new ACMLibraryAdapter();
+		$ACMPapers = $ACM->getPapersWithAuthorName("Johnson", "10");
+
+		$ACMPapersExpected = json_decode(file_get_contents($expectedOutputFileName), true);
+		$this->assertEquals(10, sizeof($ACMPapers));
+		$this->assertEquals($ACMPapersExpected, $ACMPapers);
+
+	}
+
+	public function testInvalidGetPapersWithAuthorName()
+	{
+		$expectedOutputFileName = "unitTests/expected_output/testInvalidACMGetPapersWithAuthorNameOutput.json";
+		$ACM = new ACMLibraryAdapter();
+		$ACMPapers = $ACM->getPapersWithAuthorName("Banananana", "10");
+
+		$ACMPapersExpected = json_decode(file_get_contents($expectedOutputFileName), true);
+		$this->assertEquals($ACMPapersExpected, $ACMPapers);
 	}
 }
--- unitTests/IEEELibaryAdapterTest.php
+++ unitTests/IEEELibaryAdapterTest.php
@@ -1,12 +1,29 @@
 <?php
-//require_once "cache/CacheArtistMBIDs.php";
+require_once "vendor/phpunit/phpunit/src/Framework/Assert/Functions.php";
+require_once "ajax/IEEELibraryAdapter.php";
 
 use PHPUnit\Framework\TestCase;
 
 class IEEELibraryAdapterTest extends TestCase
 {
-	public function testFunction()
+	public function testValidGetPapersWithAuthorName()
 	{
-		
+		$expectedOutputFileName = "unitTests/expected_output/testValidGetPapersWithAuthorNameOutput.json";
+		$ieee = new IEEELibraryAdapter();
+		$ieeePapers = $ieee->getPapersWithAuthorName("Johnson", "10");
+
+		$ieeePapersExpected = json_decode(file_get_contents($expectedOutputFileName), true);
+		$this->assertEquals(10, sizeof($ieeePapers));
+		$this->assertEquals($ieeePapersExpected, $ieeePapers);
+
+	}
+	public function testInvalidGetPapersWithAuthorName()
+	{
+		$expectedOutputFileName = "unitTests/expected_output/testInvalidGetPapersWithAuthorNameOutput.json";
+		$ieee = new IEEELibraryAdapter();
+		$ieeePapers = $ieee->getPapersWithAuthorName("Banananana", "10");
+
+		$ieeePapersExpected = json_decode(file_get_contents($expectedOutputFileName), true);
+		$this->assertEquals($ieeePapersExpected, $ieeePapers);
 	}
 }
