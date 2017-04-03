a23397a2e25e7b714bf66d7313c4c42e1b353141
Timmy Malaney <tmalaney@usc.edu>
2017-04-02 19:18:41 -0700

    refactored feature files

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -40,7 +40,7 @@ class FeatureContext implements Context
 
         $this->session->start();
 
-        $this->session->visit('http://localhost:80/PaperCloud/');
+        $this->session->visit('http://localhost:80');
         $this->page = $this->session->getPage();
 
 
