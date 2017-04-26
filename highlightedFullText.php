1ff8dfd2fad87f9afcee8b6b724adb52ed679c8e
Phillip Nazarian <pmnazari@usc.edu>
2017-04-25 17:54:33 -0700

    Req. 15 done for ACM papers

--- highlightedFullText.php
+++ highlightedFullText.php
@@ -18,80 +18,61 @@ file_put_contents("fulltext.pdf", $highlightedFullText);
 		<script src="scripts/textlayer.js"></script>
 		<script>		
 			$(document).ready(function() {
-				
-				var _ = {};
-				
-				_.$canvas = $('#canvas');
-				var ctx = _.$canvas.get(0).getContext('2d');
 
 				// Render the PDF
 				PDFJS.getDocument("fulltext.pdf").then(function(pdf) {
-					pdf.getPage(1).then(function(page) {
-						
-						_.page = page;
-						
-						var viewport = page.getViewport(1);
-						_.scale = _.$canvas.get(0).height / viewport.height;
-						viewport = page.getViewport(_.scale);
-						_.$canvas.get(0).width = viewport.width;
 
-						var params = {
-							canvasContext: ctx,
-							viewport: viewport
-						};
-						page.render(params);
-						
-						// Make divs
-						page.getTextContent().then(function(tc) {
-							
-							renderTextLayer({
-								textContent: tc,
-								container: document.getElementById("overlay"),
-								viewport: viewport,
-								textDivs: [],
-								enhanceTextSelection: false
-							});
-							
-							/*
-							$('#here').empty();
-							_.titles = [];
-							var sizes = {};
-							
-							$.each(tc.items, function(index) {
-								
-								var match = /energy/i.exec(this.str);
-								
-								if (match)
-								{
-									var tx = Util.transform(viewport.transform, this.transform);
-									var fontHeight = Math.sqrt((tx[2] * tx[2]) + (tx[3] * tx[3]));
-									
-									ctx.font = (this.height * 0.95) + 'px ' + tc.styles[this.fontName].fontFamily;
-									//console.log(tc.styles);
-									
-									overlay(
-										this.transform[4] + ctx.measureText(this.str.substring(0, match.index)).width, // x
-										this.transform[5], // y
-										ctx.measureText(match[0]).width, // w
-										this.height, // h
-										'toggle-on yes-no', // style
-										function() {} // on click
-									);
-								}
-							});
-							*/
 
-							// Now, highlight the word in the divs
-							$("#overlay > div").each(function(index) {
-								
-								$(this).html($(this).text().replace(/energy/i, function(x) {
-									return "<span class='highlight'>" + x + "</span>";
-								}));
-								
-							});
+					for (var pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++)
+					{
+						(function (pageNumber) {						
 							
-						});
-					});
+							var canvas = $("<canvas height=1200>");
+							$("<div class='page' id='page_" + pageNumber + "'>")
+								.append(canvas)
+								.append("<div class='overlay' id='overlay_" + pageNumber + "'>")
+								.appendTo("#preview");
+
+							var ctx = canvas.get(0).getContext('2d');
+
+							pdf.getPage(pageNumber).then(function(page) {
+						
+								var viewport = page.getViewport(1);
+								var scale = canvas.get(0).height / viewport.height;
+								viewport = page.getViewport(scale);
+								canvas.get(0).width = viewport.width;
+
+								var params = {
+									canvasContext: ctx,
+									viewport: viewport
+								};
+								page.render(params);
+						
+								// Make divs
+								page.getTextContent().then(function(tc) {
+							
+									renderTextLayer({
+										textContent: tc,
+										container: document.getElementById("overlay_" + pageNumber),
+										viewport: viewport,
+										textDivs: [],
+										enhanceTextSelection: false
+									});
+							
+									// Now, highlight the word in the divs
+									$("#overlay_" + pageNumber + " > div").each(function(index) {
+								
+										$(this).html($(this).text().replace(<?php echo '/' . $_POST["word"] . '/i'; ?>, function(x) {
+											return "<span class='highlight'>" + x + "</span>";
+										}));
+								
+									});
+							
+								});
+							});
+						}) (pageNumber);
+					}
+
 				});
 			});
 		</script>
@@ -99,10 +80,7 @@ file_put_contents("fulltext.pdf", $highlightedFullText);
 		<link rel=STYLESHEET href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" type="text/css" />
 	</head>
 	<body>
-		<div id="preview">
-			<canvas id="canvas" height=1200 ></canvas>
-			<div id="overlay"></div>
-		</div>
+		<div id="preview"></div>
 	</body>
 </html>
 
