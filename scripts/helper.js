13eff4f31e98c8f1c98a798e01328d2c06206706
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-08 23:52:52 +0800

    Delete scripts directory

--- scripts/helper.js
+++
@@ -1,7 +0,0 @@
-
-// returns a RegExp object that can be used to match the word in a lyrics
-function regexForWord(test) {
-	
-	var regex = new RegExp("\\b" + test + "\\b", "ig");	
-	return regex;
-}
