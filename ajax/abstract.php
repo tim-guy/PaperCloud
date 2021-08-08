f44ba56db8ff51f8e741a31efcd9c30281c5e3fd
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-08 23:28:12 +0800

    Delete ajax directory

--- ajax/abstract.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-Type: text/plain');
-
-require_once "LibraryAdapter.php";
-
-$abstract = LibraryAdapter::getAbstractForPaperFromLibrary($_POST);
-
-echo $abstract ? $abstract : "Abstract not found!";
