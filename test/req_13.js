38440e33031cdba5c85a872e8097ae731d9fd3fb
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-08 23:53:31 +0800

    Delete test directory

--- test/req_13.js
+++
@@ -1,9 +0,0 @@
-var include = require('../scripts/include.js');
-
-QUnit.test("testParseDataURL", function( assert ) {
-  
-	var dataURL = "data:image/png;base64,TESTDATA123";
-	var data = include.parseDataURL(dataURL);
-	
-	assert.equal("TESTDATA123", data);
-});
