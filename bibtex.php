08696ebeee393a53fdec09e0b7f1cb8d2d18ac32
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-09 08:53:56 +0800

    Delete bibtex.php

--- bibtex.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-Type: text/plain');
-
-require_once "ajax/LibraryAdapter.php";
-
-$bibtex = LibraryAdapter::getBibtexForPaperFromLibrary($_POST);
-
-echo $bibtex ? $bibtex : "BibTeX not found!";
